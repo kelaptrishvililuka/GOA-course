fa94161bfb2855288069781af9d9522936d18065
kelaptrishvililuka <kelaptrishvililuka91@gmail.com>
2024-09-29 22:57:42 +0400

    Add files via upload

---
+++ level 04/Homework/level 4.py
@@ -0,0 +1,17 @@
+# 1. firstName = input("luka")
+# lastname = input("xiblashvili")
+# print("Hello" + firstName + " " + lastname)
+
+2 ,age = 15
+print(age + 10)
+
+3 , # Calculate the average of 5 numbers 
+ 
+# Define the numbers 
+numbers = [10, 20, 30, 40, 50] 
+
+4, #case-sensitive This means that it treats uppercase and lowercase letters differently.
+   #Snake case is a naming convention where a developer replaces spaces between words with an underscore.
+
+
+5, 
